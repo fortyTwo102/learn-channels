bbb30cc2850c436ae56c200c786f6e18b453c93c
Farooq Ansari <farooq.ansari21297@gmail.com>
2022-09-29 02:03:04 +0530

    room is created instantaneously now

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,23 @@
+from turtle import xcor
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from chat.views import room
+
+# helpers
+import random, string
+
+# generate random room name
+def generate_room_name():
+    global room_name
+    room_name = ''.join(random.choices(string.ascii_letters + string.digits, k=6)) + "/"
+
+    return room_name
+
+room_name = ''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
+    path('chat/', lambda request: redirect(generate_room_name(), permanent=False)),
+    path('chat/<str:room_name>/', include("chat.urls")),
 ]
